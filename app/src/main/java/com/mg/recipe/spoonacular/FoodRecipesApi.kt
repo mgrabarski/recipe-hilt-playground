6ebf35c702e6f41be730d14a424b0c5b8b8f8598
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-02 12:16:04 +0200

    Added food recipes api

---
+++ app/src/main/java/com/mg/recipe/spoonacular/FoodRecipesApi.kt
@@ -0,0 +1,12 @@
+package com.mg.recipe.spoonacular
+
+import com.mg.recipe.spoonacular.data.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(@QueryMap queries: Map<String, String>): Response<FoodRecipe>
+}
