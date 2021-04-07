269b520ba961c519486ac3b430cd4d9d4a2306f1
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-07 14:12:05 +0200

    Start adding food recipes repository

---
+++ app/src/main/java/com/mg/recipe/repo/FoodRecipesRepository.kt
@@ -0,0 +1,11 @@
+package com.mg.recipe.repo
+
+import com.mg.recipe.repo.network.RemoteDataSource
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+
+@ActivityRetainedScoped
+class FoodRecipesRepository @Inject constructor(
+    private val remote: RemoteDataSource
+) {
+}
