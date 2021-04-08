dbbf359e08a68c650880b48267ca07e8ac79b7f9
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-08 12:37:37 +0200

    Added binding adapter to apply vegan green color

--- app/src/main/java/com/mg/recipe/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/mg/recipe/bindingadapters/RecipesRowBinding.kt
@@ -1,7 +1,11 @@
 package com.mg.recipe.bindingadapters
 
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.mg.recipe.R
 
 class RecipesRowBinding {
 
@@ -17,6 +21,18 @@ class RecipesRowBinding {
         @BindingAdapter("setNumberOfMinutes")
         fun setNumberOfMinutes(view: TextView, minutes: Int) {
             view.text = minutes.toString()
+        }
+
+        @JvmStatic
+        @BindingAdapter("applyVeganColor")
+        fun applyVeganColor(view: View, vegan: Boolean) {
+            if (vegan) {
+                val greenColor = ContextCompat.getColor(view.context, R.color.green)
+                when (view) {
+                    is TextView -> view.setTextColor(greenColor)
+                    is ImageView -> view.setColorFilter(greenColor)
+                }
+            }
         }
     }
 }
