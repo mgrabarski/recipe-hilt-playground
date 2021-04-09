14fb967adcfd5b96a75b6f2ae25897d987aa22c4
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-09 12:05:05 +0200

    Added local data source to repository

--- app/src/main/java/com/mg/recipe/repo/FoodRecipesRepository.kt
+++ app/src/main/java/com/mg/recipe/repo/FoodRecipesRepository.kt
@@ -1,14 +1,19 @@
 package com.mg.recipe.repo
 
 import com.mg.recipe.repo.network.RemoteDataSource
+import com.mg.recipe.repo.sources.LocalDataSource
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
 @ActivityRetainedScoped
 class FoodRecipesRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource
 ) {
 
     val remote
         get() = remoteDataSource
+
+    val local
+        get() = localDataSource
 }
