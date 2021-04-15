3a592da357ac2ebd912e3a36ce8fbec5e036fe2c
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-15 14:09:11 +0200

    Added recipe bottom sheet

---
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/bottomsheet/RecipeBottomSheetFragment.kt
@@ -0,0 +1,17 @@
+package com.mg.recipe.ui.fragments.recipes.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mg.recipe.R
+
+class RecipeBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_recipes_bottom_sheet, container, false)
+}
