2675ef0ab67a602af1a8b3314be4971234000861
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-15 14:30:50 +0200

    Move constants

--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/RecipesViewModel.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/RecipesViewModel.kt
@@ -4,15 +4,18 @@ import android.app.Application
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
 import com.mg.recipe.repo.network.*
+import com.mg.recipe.ui.fragments.recipes.bottomsheet.consts.DEFAULT_DIET_TYPE
+import com.mg.recipe.ui.fragments.recipes.bottomsheet.consts.DEFAULT_MEAL_TYPE
+import com.mg.recipe.ui.fragments.recipes.bottomsheet.consts.DEFAULT_RECIPES_NUMBER
 
 class RecipesViewModel @ViewModelInject constructor(
     application: Application
 ) : AndroidViewModel(application) {
 
     fun getRequestQueries() = mapOf(
-        QUERY_NUMBER to "50",
-        QUERY_TYPE to "main course",
-        QUERY_DIET to "gluten free",
+        QUERY_NUMBER to DEFAULT_RECIPES_NUMBER,
+        QUERY_TYPE to DEFAULT_MEAL_TYPE,
+        QUERY_DIET to DEFAULT_DIET_TYPE,
         QUERY_ADD_RECIPE_INFORMATION to "true",
         QUERY_FILL_INGREDIENTS to "true"
     )
