819d8f8b4deffc3ceb96e05ea883c0d85f9bd67d
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-15 14:56:21 +0200

    Use data store in recipe view model

--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/RecipesViewModel.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/RecipesViewModel.kt
@@ -3,20 +3,57 @@ package com.mg.recipe.ui.fragments.recipes
 import android.app.Application
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.mg.recipe.repo.DataStoreRepository
 import com.mg.recipe.repo.network.*
 import com.mg.recipe.ui.fragments.recipes.bottomsheet.consts.DEFAULT_DIET_TYPE
 import com.mg.recipe.ui.fragments.recipes.bottomsheet.consts.DEFAULT_MEAL_TYPE
 import com.mg.recipe.ui.fragments.recipes.bottomsheet.consts.DEFAULT_RECIPES_NUMBER
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class RecipesViewModel @ViewModelInject constructor(
-    application: Application
+    application: Application,
+    private val foodDataStoreRepository: DataStoreRepository
 ) : AndroidViewModel(application) {
 
-    fun getRequestQueries() = mapOf(
-        QUERY_NUMBER to DEFAULT_RECIPES_NUMBER,
-        QUERY_TYPE to DEFAULT_MEAL_TYPE,
-        QUERY_DIET to DEFAULT_DIET_TYPE,
-        QUERY_ADD_RECIPE_INFORMATION to "true",
-        QUERY_FILL_INGREDIENTS to "true"
-    )
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = foodDataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(
+        selectedMealType: String,
+        selectedMealTypeId: Int,
+        selectedDietType: String,
+        selectedDietTypeId: Int
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            foodDataStoreRepository.saveMealAndDietType(
+                selectedMealType,
+                selectedMealTypeId,
+                selectedDietType,
+                selectedDietTypeId
+            )
+        }
+    }
+
+    fun getRequestQueries(): Map<String, String> {
+
+        viewModelScope.launch {
+            readMealAndDietType.collect {
+                mealType = it.selectedMealType
+                dietType = it.selectedDietType
+            }
+        }
+
+        return mapOf(
+            QUERY_NUMBER to DEFAULT_RECIPES_NUMBER,
+            QUERY_TYPE to mealType,
+            QUERY_DIET to dietType,
+            QUERY_ADD_RECIPE_INFORMATION to "true",
+            QUERY_FILL_INGREDIENTS to "true"
+        )
+    }
 }
--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/bottomsheet/consts/Constants.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/bottomsheet/consts/Constants.kt
@@ -1,5 +1,5 @@
 package com.mg.recipe.ui.fragments.recipes.bottomsheet.consts
 
-const val DEFAULT_RECIPES_NUMBER = 50
+const val DEFAULT_RECIPES_NUMBER = "50"
 const val DEFAULT_MEAL_TYPE = "main course"
 const val DEFAULT_DIET_TYPE = "gluten free"
