599f5189e02bab78fe0d65680a28a1f7223c6f7f
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-16 13:25:03 +0200

    Use arge from navigation component

--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/RecipesFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.mg.recipe.R
 import com.mg.recipe.databinding.FragmentRecipesBinding
 import com.mg.recipe.ext.observeOnce
@@ -21,6 +22,8 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private val args by navArgs<RecipesFragmentArgs>()
 
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
