e7df45eb7403376c895e6d6b1d8baf1b9ff00984
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-19 14:36:50 +0200

    Added network listener

---
+++ app/src/main/java/com/mg/recipe/components/NetworkListener.kt
@@ -0,0 +1,44 @@
+package com.mg.recipe.components
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+
+@ExperimentalCoroutinesApi
+class NetworkListener : ConnectivityManager.NetworkCallback() {
+
+    private val isNetworkAvailable = MutableStateFlow(false)
+
+    fun checkNetworkAvailability(context: Context): MutableStateFlow<Boolean> {
+        val connectivityManager =
+            context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.registerDefaultNetworkCallback(this)
+
+        var isConnected = false
+
+        connectivityManager.allNetworks.forEach { network ->
+            val networkCapability = connectivityManager.getNetworkCapabilities(network)
+            networkCapability?.let {
+                if (it.hasCapability(NET_CAPABILITY_INTERNET)) {
+                    isConnected = true
+                    return@forEach
+                }
+            }
+        }
+
+        isNetworkAvailable.value = isConnected
+        return isNetworkAvailable
+    }
+
+    override fun onAvailable(network: Network) {
+        super.onAvailable(network)
+    }
+
+    override fun onLost(network: Network) {
+        super.onLost(network)
+    }
+}
