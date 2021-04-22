56d5b0667436fc2935c32dccaababbc9a18dc40b
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-22 13:37:57 +0200

    Added empty recipe details activity

---
+++ app/src/main/java/com/mg/recipe/ui/RecipeDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.mg.recipe.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mg.recipe.R
+
+class RecipeDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_details)
+    }
+}
