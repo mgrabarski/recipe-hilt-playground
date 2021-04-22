bafd0cb0d5f7a77de072d4c19c2be46ede7b6cfb
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-22 14:19:35 +0200

    Init view pager

--- app/src/main/java/com/mg/recipe/ui/RecipeDetailsActivity.kt
+++ app/src/main/java/com/mg/recipe/ui/RecipeDetailsActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.navArgs
 import com.mg.recipe.R
 import com.mg.recipe.databinding.ActivityRecipeDetailsBinding
+import com.mg.recipe.ui.fragments.recipes.details.adapters.PagerAdapter
+import com.mg.recipe.ui.fragments.recipes.details.fragments.ingredients.IngredientsFragment
+import com.mg.recipe.ui.fragments.recipes.details.fragments.instructions.InstructionsFragment
+import com.mg.recipe.ui.fragments.recipes.details.fragments.overview.OverviewFragment
 
 class RecipeDetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<RecipeDetailsActivityArgs>()
 
     private lateinit var binding: ActivityRecipeDetailsBinding
 
@@ -19,6 +26,29 @@ class RecipeDetailsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        val fragments = arrayListOf(
+            OverviewFragment(),
+            IngredientsFragment(),
+            InstructionsFragment()
+        )
+        val titles = arrayListOf(
+            getString(R.string.overview),
+            getString(R.string.ingredients),
+            getString(R.string.instructions)
+        )
+
+        val bundle = Bundle().apply {
+            putSerializable("recipeBundle", args.result)
+        }
+
+        val pagerAdapter = PagerAdapter(bundle, fragments, titles, supportFragmentManager)
+        binding.viewPager.adapter = pagerAdapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
