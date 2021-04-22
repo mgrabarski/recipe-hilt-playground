b648c457bbe1a83774cae45babfc4b4d15a60ee6
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-22 14:05:02 +0200

    Details toolbar

--- app/src/main/java/com/mg/recipe/ui/RecipeDetailsActivity.kt
+++ app/src/main/java/com/mg/recipe/ui/RecipeDetailsActivity.kt
@@ -1,13 +1,30 @@
 package com.mg.recipe.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.mg.recipe.R
+import com.mg.recipe.databinding.ActivityRecipeDetailsBinding
 
 class RecipeDetailsActivity : AppCompatActivity() {
 
+    private lateinit var bining: ActivityRecipeDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe_details)
+        bining = ActivityRecipeDetailsBinding.inflate(layoutInflater)
+        setContentView(bining.root)
+
+        setSupportActionBar(bining.toolbar)
+        bining.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
