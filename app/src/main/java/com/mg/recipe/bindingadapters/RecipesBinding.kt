8dbd2e9c2478d869f403eb667acb38b67c2267f3
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-23 13:51:13 +0200

    Cleanup

--- app/src/main/java/com/mg/recipe/bindingadapters/RecipesBinding.kt
+++ app/src/main/java/com/mg/recipe/bindingadapters/RecipesBinding.kt
@@ -9,41 +9,33 @@ import com.mg.recipe.repo.network.NetworkResult
 import com.mg.recipe.repo.network.NetworkResult.*
 import com.mg.recipe.spoonacular.data.models.FoodRecipe
 
-class RecipesBinding {
+@BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
+fun errorImageViewVisibility(
+    imageView: ImageView,
+    apiResponse: NetworkResult<FoodRecipe>?,
+    database: List<Recipe>?
+) {
+    if (apiResponse is Error && database.isNullOrEmpty()) {
+        imageView.visibility = View.VISIBLE
+    } else if (apiResponse is Loading) {
+        imageView.visibility = View.INVISIBLE
+    } else if (apiResponse is Success) {
+        imageView.visibility = View.INVISIBLE
+    }
+}
 
-    companion object {
-
-        @JvmStatic
-        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
-        fun errorImageViewVisibility(
-            imageView: ImageView,
-            apiResponse: NetworkResult<FoodRecipe>?,
-            database: List<Recipe>?
-        ) {
-            if (apiResponse is Error && database.isNullOrEmpty()) {
-                imageView.visibility = View.VISIBLE
-            } else if (apiResponse is Loading) {
-                imageView.visibility = View.INVISIBLE
-            } else if (apiResponse is Success) {
-                imageView.visibility = View.INVISIBLE
-            }
-        }
-
-        @JvmStatic
-        @BindingAdapter("readApiResponseText", "readDatabaseText", requireAll = true)
-        fun errorTextViewVisibility(
-            textView: TextView,
-            apiResponse: NetworkResult<FoodRecipe>?,
-            database: List<Recipe>?
-        ) {
-            if (apiResponse is Error && database.isNullOrEmpty()) {
-                textView.visibility = View.VISIBLE
-                textView.text = apiResponse.message.toString()
-            } else if (apiResponse is Loading) {
-                textView.visibility = View.INVISIBLE
-            } else if (apiResponse is Success) {
-                textView.visibility = View.INVISIBLE
-            }
-        }
+@BindingAdapter("readApiResponseText", "readDatabaseText", requireAll = true)
+fun errorTextViewVisibility(
+    textView: TextView,
+    apiResponse: NetworkResult<FoodRecipe>?,
+    database: List<Recipe>?
+) {
+    if (apiResponse is Error && database.isNullOrEmpty()) {
+        textView.visibility = View.VISIBLE
+        textView.text = apiResponse.message.toString()
+    } else if (apiResponse is Loading) {
+        textView.visibility = View.INVISIBLE
+    } else if (apiResponse is Success) {
+        textView.visibility = View.INVISIBLE
     }
 }
