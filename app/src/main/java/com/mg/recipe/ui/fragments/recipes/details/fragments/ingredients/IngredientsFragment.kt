8591d0746c2ece94c609ac756800302c39270fc0
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-23 13:58:17 +0200

    Added ingredients layout

--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/ingredients/IngredientsFragment.kt
@@ -1,6 +1,17 @@
 package com.mg.recipe.ui.fragments.recipes.details.fragments.ingredients
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mg.recipe.R
 
 class IngredientsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_recipe_ingredients, container, false)
 }
