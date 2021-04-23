8060b0e8e7dd1ed867fc69d39d0f4a86877ecaa9
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-23 14:41:30 +0200

    Convert to binding

--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/ingredients/IngredientsFragment.kt
@@ -5,13 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.mg.recipe.R
+import com.mg.recipe.databinding.FragmentRecipeIngredientsBinding
+import com.mg.recipe.ui.fragments.recipes.details.fragments.ingredients.adapters.IngredientsAdapter
 
 class IngredientsFragment : Fragment() {
+
+    private val adapter = IngredientsAdapter()
+
+    private var _binding: FragmentRecipeIngredientsBinding? = null
+    private val binding: FragmentRecipeIngredientsBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_recipe_ingredients, container, false)
+    ): View {
+        _binding = FragmentRecipeIngredientsBinding.inflate(inflater, container, false)
+
+        binding.ingredientsRecyclerView.adapter = adapter
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
