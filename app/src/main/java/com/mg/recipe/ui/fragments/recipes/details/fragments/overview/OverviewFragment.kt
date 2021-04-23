d2bcd6dd03a382b77773b37a8343c5f60ad6de19
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-23 13:30:43 +0200

    Convert to binding

--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/overview/OverviewFragment.kt
@@ -5,13 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.mg.recipe.R
+import com.mg.recipe.databinding.FragmentRecipeOverviewBinding
 
 class OverviewFragment : Fragment() {
+
+    private var _binding: FragmentRecipeOverviewBinding? = null
+    private val binding: FragmentRecipeOverviewBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_recipe_overview, container, false)
+    ): View = FragmentRecipeOverviewBinding.inflate(inflater, container, false).root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
