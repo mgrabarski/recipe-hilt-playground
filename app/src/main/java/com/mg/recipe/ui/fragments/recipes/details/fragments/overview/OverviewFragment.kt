e1e13bc471b643e5816d5813b4278996ebb727bc
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-23 13:27:33 +0200

    Overview fragment layout

--- app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/recipes/details/fragments/overview/OverviewFragment.kt
@@ -1,6 +1,17 @@
 package com.mg.recipe.ui.fragments.recipes.details.fragments.overview
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mg.recipe.R
 
 class OverviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_recipe_overview, container, false)
 }
