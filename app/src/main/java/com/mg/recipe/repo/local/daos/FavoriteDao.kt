945de20e2ae2794ada78cb26296caa00cd197725
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-26 14:27:23 +0200

    Added queries

--- app/src/main/java/com/mg/recipe/repo/local/daos/FavoriteDao.kt
+++ app/src/main/java/com/mg/recipe/repo/local/daos/FavoriteDao.kt
@@ -1,13 +1,22 @@
 package com.mg.recipe.repo.local.daos
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.*
+import com.mg.recipe.repo.local.FAVORITE_TABLE
 import com.mg.recipe.repo.local.entities.Favorite
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(favorite: Favorite)
+
+    @Query("SELECT * FROM $FAVORITE_TABLE")
+    fun readAllFavorites(): Flow<List<Favorite>>
+
+    @Delete
+    suspend fun delete(favorite: Favorite)
+
+    @Query("DELETE FROM $FAVORITE_TABLE")
+    suspend fun deleteAll()
 }
