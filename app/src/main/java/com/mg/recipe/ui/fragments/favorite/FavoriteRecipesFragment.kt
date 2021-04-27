e9961a11831416f50ea86810efd297fd8d2d8063
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-27 13:02:15 +0200

    Handle favorites recipes fragment

--- app/src/main/java/com/mg/recipe/ui/fragments/favorite/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/mg/recipe/ui/fragments/favorite/FavoriteRecipesFragment.kt
@@ -5,14 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.mg.recipe.R
+import androidx.fragment.app.viewModels
+import com.mg.recipe.databinding.FragmentFavoriteReceipesBinding
+import com.mg.recipe.ui.fragments.MainViewModel
+import com.mg.recipe.ui.fragments.favorite.adapter.FavoriteRecipesAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
 
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
+
+    private var _binding: FragmentFavoriteReceipesBinding? = null
+    private val binding: FragmentFavoriteReceipesBinding
+        get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorite_receipes, container, false)
+    ): View {
+        _binding = FragmentFavoriteReceipesBinding.inflate(inflater, container, false)
+        binding.apply {
+            lifecycleOwner = this@FavoriteRecipesFragment
+            mainViewModel = this@FavoriteRecipesFragment.mainViewModel
+            mAdapter = this@FavoriteRecipesFragment.mAdapter
+        }
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
